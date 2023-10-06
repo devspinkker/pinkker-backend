ffbcb8bdc6e24b34fb177e7a46730e7010324de9
Bruno Pereyra <brunopereyra459@gmail.com>
2023-10-06 09:49:32 -0300

    redirectURL para PINKKER

--- pkg/OAuth2/configOAuth2/configOAuth2.go
+++ pkg/OAuth2/configOAuth2/configOAuth2.go
@@ -15,7 +15,7 @@ func LoadConfig() *oauth2.Config {
 	config := oauth2.Config{
 		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
 		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
-		RedirectURL:  "http://localhost:3000",
+		RedirectURL:  "http://localhost:3000/plataform/OAuth2callback",
 		Scopes: []string{
 			"https://www.googleapis.com/auth/userinfo.email",
 			"https://www.googleapis.com/auth/userinfo.profile",
