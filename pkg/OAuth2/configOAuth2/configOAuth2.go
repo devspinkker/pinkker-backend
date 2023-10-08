0bd0d9f281a7529c443677576e53069ed093855f
Bruno Pereyra <brunopereyra459@gmail.com>
2023-10-08 15:50:11 -0300

    cambios en auth2 para el nuevo netli

--- pkg/OAuth2/configOAuth2/configOAuth2.go
+++ pkg/OAuth2/configOAuth2/configOAuth2.go
@@ -15,7 +15,7 @@ func LoadConfig() *oauth2.Config {
 	config := oauth2.Config{
 		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
 		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
-		RedirectURL:  "https://spiffy-muffin-f43463.netlify.app/plataform/OAuth2callback",
+		RedirectURL:  os.Getenv("GOOGLE_RedirectURL"),
 		Scopes: []string{
 			"https://www.googleapis.com/auth/userinfo.email",
 			"https://www.googleapis.com/auth/userinfo.profile",
