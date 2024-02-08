61b7504ef0e0f1ab6fd00bbb194998f27e56cd84
Bruno Pereyra <brunopereyra459@gmail.com>
2024-02-08 16:35:13 -0300

    clips mas visitados y streams mas vistos, end points

--- pkg/helpers/resend.go
+++ pkg/helpers/resend.go
@@ -8,26 +8,28 @@ import (
 )
 
 func ResendConfirmMail(code, To string) error {
-	fmt.Println(To)
 	apikey := config.ResendApi()
 	client := resend.NewClient(apikey)
+	RESENDDOMINIO := config.ResendDominio()
 	params := &resend.SendEmailRequest{
-		From:    "onboarding@resend.dev",
+		From:    RESENDDOMINIO,
 		To:      []string{To},
 		Subject: "email confirmation code",
 		Html:    "<p>codigo de confirmacion</p>" + code,
 	}
 	_, err := client.Emails.Send(params)
+	fmt.Println(err)
 	return err
 }
 
 func ResendRecoverPassword(code, To string) error {
-	fmt.Println(To)
+	// http://vps-acad4de5.vps.ovh.ca/
 	html := "<a href='http://localhost:3000/user/password-reset?reset_token=" + code + "'target='_blank'><button style='background-color:blue; color:white;'>restablecer contraseña</button></a>"
 	apikey := config.ResendApi()
+	RESENDDOMINIO := config.ResendDominio()
 	client := resend.NewClient(apikey)
 	params := &resend.SendEmailRequest{
-		From:    "onboarding@resend.dev",
+		From:    RESENDDOMINIO,
 		To:      []string{To},
 		Subject: "email confirmation code",
 		Html:    html,
