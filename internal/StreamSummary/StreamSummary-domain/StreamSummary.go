9256815358e52668c249c634696ad745cc42aa5e
Bruno Pereyra <brunopereyra459@gmail.com>
2024-07-16 19:19:03 -0300

    end point get GeStreamSummaries

--- internal/StreamSummary/StreamSummary-domain/StreamSummary.go
+++ internal/StreamSummary/StreamSummary-domain/StreamSummary.go
@@ -22,6 +22,30 @@ type StreamSummary struct {
 	StartFollowersCount  int                `json:"StartFollowersCount" bson:"StartFollowersCount"`
 	StartSubsCount       int                `json:"StartSubsCount" bson:"StartSubsCount"`
 }
+type StreamSummaryGet struct {
+	Title                string             `json:"Title" bson:"Title"`
+	StreamThumbnail      string             `json:"StreamThumbnail" bson:"StreamThumbnail"`
+	ID                   primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	EndOfStream          time.Time          `json:"EndOfStream" bson:"EndOfStream"`
+	AverageViewers       int                `json:"AverageViewers" bson:"AverageViewers"`
+	AverageViewersByTime map[string]int     `json:"AverageViewersByTime" bson:"AverageViewersByTime"`
+	MaxViewers           int                `json:"MaxViewers" bson:"MaxViewers"`
+	NewFollowers         int                `json:"NewFollowers" bson:"NewFollowers"`
+	NewSubscriptions     int                `json:"NewSubscriptions" bson:"NewSubscriptions"`
+	Advertisements       int                `json:"Advertisements" bson:"Advertisements"`
+	StartOfStream        time.Time          `json:"StartOfStream" bson:"StartOfStream"`
+	StreamerID           primitive.ObjectID `json:"StreamerID" bson:"StreamerID"`
+	StartFollowersCount  int                `json:"StartFollowersCount" bson:"StartFollowersCount"`
+	StartSubsCount       int                `json:"StartSubsCount" bson:"StartSubsCount"`
+	UserInfo             UserInfo           `json:"UserInfo" bson:"UserInfo"`
+}
+
+type UserInfo struct {
+	Avatar   string `json:"Avatar" bson:"Avatar"`
+	FullName string `json:"FullName" bson:"FullName"`
+	NameUser string `json:"NameUser" bson:"NameUser"`
+}
+
 type UpdateStreamSummary struct {
 	AverageViewers   int `json:"AverageViewers" bson:"AverageViewers"`
 	MaxViewers       int `json:"MaxViewers" bson:"MaxViewers"`
