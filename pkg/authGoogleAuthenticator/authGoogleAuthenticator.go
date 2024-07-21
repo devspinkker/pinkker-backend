cb3776cfd935b06728f5512b27e55708434df2ac
Bruno Pereyra <brunopereyra459@gmail.com>
2024-07-21 20:36:02 -0300

    addSubscription donation  y withdraw se le agrego el middleware TOTPAuth

--- pkg/authGoogleAuthenticator/authGoogleAuthenticator.go
+++ pkg/authGoogleAuthenticator/authGoogleAuthenticator.go
@@ -1,6 +1,9 @@
 package authGoogleAuthenticator
 
 import (
+	"fmt"
+	"net/url"
+
 	"github.com/pquerna/otp/totp"
 )
 
@@ -14,8 +17,20 @@ func GenerateKey(accountName, nameUser string) (string, string, error) {
 		return "", "", err
 	}
 
-	url := key.URL() + "&issuer=Pinkker.tv:" + nameUser
-	return key.Secret(), url, nil
+	otpURL := url.URL{
+		Scheme: "otpauth",
+		Host:   "totp",
+		Path:   fmt.Sprintf("%s:%s", url.QueryEscape("Pinkker.tv"), url.QueryEscape(nameUser)),
+	}
+	params := url.Values{}
+	params.Add("secret", key.Secret())
+	params.Add("issuer", "Pinkker.tv")
+	params.Add("algorithm", "SHA1")
+	params.Add("digits", "6")
+	params.Add("period", "30")
+	otpURL.RawQuery = params.Encode()
+
+	return key.Secret(), otpURL.String(), nil
 }
 
 // ValidateCode validates the TOTP code
