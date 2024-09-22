143aac8539bca4dd5d6d9607979e3c3bf3df84d9
Bruno Pereyra <brunopereyra459@gmail.com>
2024-09-22 12:17:37 -0300

    get State pendig

--- internal/advertisements/advertisements-interface/advertisements.handler.go
+++ internal/advertisements/advertisements-interface/advertisements.handler.go
@@ -169,7 +169,7 @@ func (s *AdvertisementsRepository) RemovePendingAds(c *fiber.Ctx) error {
 		})
 	}
 
-	var req advertisements.AdvertisementGet
+	var req advertisements.AcceptPendingAds
 
 	if err := c.BodyParser(&req); err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
@@ -178,8 +178,7 @@ func (s *AdvertisementsRepository) RemovePendingAds(c *fiber.Ctx) error {
 		})
 	}
 
-	nameUser := c.Query("nameUser", "1")
-	err := s.Servise.RemovePendingAds(idValueObj, req, nameUser)
+	err := s.Servise.RemovePendingAds(idValueObj, req)
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
 			"message": "StatusInternalServerError",
