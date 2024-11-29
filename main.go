6df1afcc39159ea70691fba923697bf61a75b5b1
Bruno Pereyra <brunopereyra459@gmail.com>
2024-11-29 18:26:24 -0300

    cambios en la forma es que pedimos las notificaciones creamos documento con las nootificaciones del usuario

--- main.go
+++ main.go
@@ -40,12 +40,12 @@ func main() {
 		BodyLimit: 200 * 1024 * 1024,
 	})
 	app.Use(cors.New(
-	// cors.Config{
-	// 	AllowCredentials: true,
-	// 	AllowOrigins:     "https://www.pinkker.tv",
-	// 	AllowHeaders:     "Origin, Content-Type, Accept, Accept-Language, Content-Length",
-	// 	AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
-	// },
+		cors.Config{
+			AllowCredentials: true,
+			AllowOrigins:     "https://www.pinkker.tv",
+			AllowHeaders:     "Origin, Content-Type, Accept, Accept-Language, Content-Length",
+			AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
+		},
 	))
 	app.Use("/ws", func(c *fiber.Ctx) error {
 		if websocket.IsWebSocketUpgrade(c) {
